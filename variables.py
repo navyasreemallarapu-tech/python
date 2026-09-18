ad4a90be8897ac0999cd3fd4e9af365d93d54636
navyasreemallarapu-tech <navyasreemallarapu@gmail.com>
2026-09-18 21:21:45 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+x = 'python'
+x = 'Mallareddy'
+y = 20
+z = 'Tirupathi'
+print(x)
+print(y)
+print(z) 
+
+x = 'product name : Ghar soaps'
+print(x)
+y = 'price : 190'
+print(y)
+z = 'pin code : 517425'
+print(z)
+h = 'location : Tirupathi'
+print(h)
